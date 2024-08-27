28092ccdcbead9964f62089cec929d7fb779fbd1
Source Academy User <No public email provided>
2024-08-27 15:52:50 +0800

    Changes made from Source Academy

--- s3-in-class.js
+++ s3-in-class.js
@@ -1,4 +1,4 @@
-import {stack, stack_frac, beside_frac, beside, circle, square, heart, show, blank, ribbon} from 'rune';
+import {stack, stack_frac, beside_frac, beside, circle, square, show, blank, ribbon} from 'rune';
 
 function moony_1(bottom_right, x_frac, y_frac) {
     return stack_frac(y_frac,
@@ -9,11 +9,21 @@ function moony_1(bottom_right, x_frac, y_frac) {
 function moony_2(n, count, rune) {
     return count === n
            ? rune
-           : moony_2(n, count + 1 , moony_1(rune, 1 / (count + 1), 1 / (count + 1) ));
+           : moony_2(n,
+                     count + 1,
+                     moony_1(rune, 1 / (count + 1), 1 / (count + 1)));
 }
 
 function moony(n) {
     return moony_2(n, 1, circle);
 }
 
-show(moony(4));
+show(moony(5));
+
+/*
+4. iterative. 
+time: O(n), theta(n), omega(n)
+space: O(1), theta(1), omega(1)
+All primitive runes and primitive operations on runes 
+consume the same amount of resources.
+*/
