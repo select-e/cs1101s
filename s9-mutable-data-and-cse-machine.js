b22d79d5c48a2643378d3c8c2041f3e19e52a520
Source Academy User <No public email provided>
2024-10-15 14:45:22 +0800

    Changes made from Source Academy

--- s9-mutable-data-and-cse-machine.js
+++ s9-mutable-data-and-cse-machine.js
@@ -8,27 +8,40 @@ only be made of existing pairs in xs. Your function must not modify the
 head of any of the existing pairs.
 */
 
-function d_filter(pred, xs) {
-    let current = xs;
-    // Set xs as the first value that returns true
-    while (!is_null(xs) && !pred(head(xs))) {
-        xs = tail(current);
-        current = xs;
+// function d_filter(pred, xs) {
+//     let current = xs;
+//     // Set xs as the first value that returns true
+//     while (!is_null(xs) && !pred(head(xs))) {
+//         xs = tail(current);
+//         current = xs;
+//     }
+//     if (!is_null(xs)) {
+//         while (!is_null(tail(current))) {
+//             if (pred(head(tail(current)))) {
+//                 // Evaluate the next element
+//                 current = tail(current);
+//             } else {
+//                 // Link head and the tail of tail
+//                 set_tail(current, tail(tail(current)));
+//             }
+//         }
+//     }
+//     return xs;
+// }
+
+// Solution:
+
+function d_filter(pred,xs) {
+    if(is_null(xs)) {
+        return xs;
+    } else if (pred(head(xs))) {
+        set_tail(xs, d_filter(pred, tail(xs)));
+        return xs;
+    } else {
+        return d_filter(pred, tail(xs));
     }
-    if (!is_null(xs)) {
-        while (!is_null(tail(current))) {
-            if (pred(head(tail(current)))) {
-                // Evaluate the next element
-                current = tail(current);
-            } else {
-                // Link head and the tail of tail
-                set_tail(current, tail(tail(current)));
-            }
-        }
-    }
-    return xs;
 }
 
-const L = list(1, 3, 4, 5, 0, 0);
+const L = list(1, 2, 3, 4, 5);
 d_filter(x => x % 2 === 0, L); // returns [2, [4, [6, [8, null]]]]
-//L; // What is L now?
+// L; // What is L now?
