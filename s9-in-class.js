697e181e3dc46e7987457e2f27574bec4dbf1970
Source Academy User <No public email provided>
2024-10-15 15:08:01 +0800

    Changes made from Source Academy

--- s9-in-class.js
+++ s9-in-class.js
@@ -1,12 +1,14 @@
-let counted = list();
-
 function count_pairs(x) {
-    if (!is_pair(x) || !is_null(member(x, counted))) {
-        return 0;
-    } else {
-        counted = append(counted, list(x));
-        return 1 + count_pairs(head(x)) + count_pairs(tail(x));
+    let counted = list();
+    function helper(x) {
+        if (!is_pair(x) || !is_null(member(x, counted))) {
+            return 0;
+        } else {
+            counted = append(counted, list(x));
+            return 1 + helper(head(x)) + helper(tail(x));
+        }
     }
+    return helper(x);
 }
 
 const p = pair(null, null);
